8daf92612bc9bd5fb2cbcab160a8b7fb3f23ef35
chenzixuan <2548736054@qq.com>
2020-03-18 15:35:33 +0800

    commit firt by idea

--- src/main/java/com/segi/test/GitTest/Apple.java
+++ src/main/java/com/segi/test/GitTest/Apple.java
@@ -3,5 +3,6 @@ package com.segi.test.GitTest;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("GitEclipseTest");
+		System.out.println("edit by idea");
 	}
 }
---
+++ src/main/java/com/segi/test/GitTest/Phone.java
@@ -0,0 +1,11 @@
+package com.segi.test.GitTest;
+
+/**
+ * @author chenzx
+ * @date 2020-03-18 下午 3:34
+ */
+public class Phone {
+    public static void main(String[] args) {
+        System.out.println("edit by idea");
+    }
+}
