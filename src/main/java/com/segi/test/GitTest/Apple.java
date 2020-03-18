232f9ede74505099697fc6971832d61f0c687cf6
chenzixuan <2548736054@qq.com>
2020-03-18 14:17:43 +0800

    edit by Test

--- src/main/java/com/segi/test/GitTest/Apple.java
+++ src/main/java/com/segi/test/GitTest/Apple.java
@@ -1,5 +1,7 @@
 package com.segi.test.GitTest;
 
 public class Apple {
-
+	public static void main(String[] args) {
+		System.out.println("");
+	}
 }
--- src/test/java/com/segi/test/GitTest/AppleTest.java
+++ src/test/java/com/segi/test/GitTest/AppleTest.java
@@ -1,5 +1,7 @@
 package com.segi.test.GitTest;
 
 public class AppleTest {
-
+	public static void main(String[] args) {
+		System.out.println("");
+	}
 }
