edc9cab8d5ddbb98515fe40c4d620c3e40dcfef6
chenzixuan <2548736054@qq.com>
2020-03-18 15:43:55 +0800

    add mi by idea --dev_idea

---
+++ src/main/java/com/segi/test/GitTest/Mi.java
@@ -0,0 +1,11 @@
+package com.segi.test.GitTest;
+
+/**
+ * @author chenzx
+ * @date 2020-03-18 下午 3:42
+ */
+public class Mi {
+    public static void main(String[] args) {
+        System.out.println("xiao mi");
+    }
+}
